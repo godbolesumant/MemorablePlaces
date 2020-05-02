740d0f7a54fe7a5ffc8d4da10fa3fd1422c1fe25
godbolesumant <godbole.sumant@gmail.com>
2020-05-02 20:27:36 +0530

    Maps Activity added

--- app/src/main/java/sumant/androidapplication/memorableplaces/MainActivity.java
+++ app/src/main/java/sumant/androidapplication/memorableplaces/MainActivity.java
@@ -2,13 +2,47 @@ package sumant.androidapplication.memorableplaces;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    // onrequest method
+
+    // onlongpressmethod
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+
+        final ArrayList<String>places =new ArrayList<String>();
+
+        places.add("+ Click to add favourite place");
+
+        ArrayAdapter arrayAdapter =new ArrayAdapter(this, android.R.layout.simple_list_item_1, places);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                intent.putExtra("placeNumber", position);
+                startActivity(intent);
+            }
+        });
+
     }
 }
